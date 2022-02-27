4e4f6b6b80246b2c09de5f4ebe0f6b1a760a7e97
ISXDora <isinhasena@gmail.com>
2022-02-27 11:17:22 -0300

    entendendo estado do componente

--- chapter-I/01-github-explorer/src/App.jsx
+++ chapter-I/01-github-explorer/src/App.jsx
@@ -1,11 +1,14 @@
 import {React} from 'react';
 import '../public/global.scss'
-
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 
 export function App(){
 
     return (
+        <>
         <RepositoryList/>
+        <Counter/>
+        </>
     )
 }
