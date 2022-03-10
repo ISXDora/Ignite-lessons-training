53ce34981f1f4a14fd0a9df547b31af5482023e7
ISXDora <isinhasena@gmail.com>
2022-03-09 23:28:12 -0300

    feat: componente wrapper de tasks Definição de layout inicial e estrutura do componente

---
+++ chapter-I/projeto-pessoal/src/components/Tasks.jsx
@@ -0,0 +1,14 @@
+import { Task } from "./Task";
+import '../components/tasks.scss'
+
+export function Tasks(){
+
+    return(
+        <section>
+            <div>
+                <Task/>
+            </div>
+        </section>
+
+    )
+}
